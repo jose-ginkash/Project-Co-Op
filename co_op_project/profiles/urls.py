93bcf8c7baef0aa8fca6e048f2aee7cea6bb07da
BonelessChachii <113223328+BonelessChachii@users.noreply.github.com>
2025-04-08 12:18:51 -0400

    Delete co_op_project directory
    deletin old shit

--- co_op_project/profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProfileDetailView, ProfileUpdateView
-
-urlpatterns = [
-    path('<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
-    path('<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile-update'),
-]
